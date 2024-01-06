5177ff2d5897bcbeda2cf2217e6669a23f382809
vinod <vinodk2757@gmail.com>
2024-01-06 14:19:05 +0530

    golang port change to 3000

--- simple-gin-server/server.go
+++ simple-gin-server/server.go
@@ -8,10 +8,11 @@ import (
 
 func main() {
 	r := gin.Default()
-	r.GET("/ping", func(c *gin.Context) {
+
+	r.GET("/", func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{
-			"message": "pong",
+			"message": "Hello World!",
 		})
 	})
-	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
+	r.Run("localhost:3000")
 }
