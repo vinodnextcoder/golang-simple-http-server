b9e097cfcc0b0cb9406be07968d6f082d7c170e0
vinod <vinodk2757@gmail.com>
2024-01-08 23:15:38 +0530

    go server

---
+++ gin-web-app/main.go
@@ -0,0 +1,21 @@
+// main.go
+package main
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	// Create a Gin router
+	router := gin.Default()
+
+	// Define a route handler
+	router.GET("/", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{"message": "Hello, You Created a Web App!"})
+	})
+
+	// Run the server on port 8080
+	router.Run(":3000")
+}
