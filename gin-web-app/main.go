6733b03289cec48997e5f9687c87abe4a672b05a
vinod <vinodk2757@gmail.com>
2024-01-09 00:06:10 +0530

    go api return array of object

--- gin-web-app/main.go
+++ gin-web-app/main.go
@@ -6,6 +6,13 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+// album represents data about a record album.
+type userDetail struct {
+	ID   string `json:"id"`
+	Name string `json:"name"`
+	Age  int    `json:"age"`
+}
+
 func helloCall(c *gin.Context) {
 	c.JSON(http.StatusOK, gin.H{"message": "Hello, You created a Web App!"})
 }
@@ -20,6 +27,16 @@ func contactCall(c *gin.Context) {
 func getUser(c *gin.Context) {
 	name := c.Param("name")
 	c.JSON(http.StatusOK, gin.H{"message": "Hello, " + name + "!"})
+}
+
+func getUsers(c *gin.Context) {
+	var users = []userDetail{
+		{ID: "1", Name: "Blue Train", Age: 56},
+		{ID: "2", Name: "Akash", Age: 11},
+		{ID: "3", Name: "pradip", Age: 56},
+	}
+
+	c.JSON(http.StatusOK, users)
 }
 
 func main() {
@@ -37,6 +54,7 @@ func main() {
 	// Add this route handler
 	// with param
 	router.GET("/user/:name", getUser)
+	router.GET("/user", getUsers)
 
 	router.Run(":3000")
 }
