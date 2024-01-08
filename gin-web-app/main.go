7579e9e88801052a3840f73ee45074ddf0bd7252
vinod <vinodk2757@gmail.com>
2024-01-08 23:31:13 +0530

    go server handler func

--- gin-web-app/main.go
+++ gin-web-app/main.go
@@ -1,4 +1,3 @@
-// main.go
 package main
 
 import (
@@ -7,15 +6,37 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+func helloCall(c *gin.Context) {
+	c.JSON(http.StatusOK, gin.H{"message": "Hello, You created a Web App!"})
+}
+
+func aboutCall(c *gin.Context) {
+	c.JSON(http.StatusOK, gin.H{"message": "About Us"})
+}
+func contactCall(c *gin.Context) {
+	c.JSON(http.StatusOK, gin.H{"message": "Contact Us"})
+}
+
+func getUser(c *gin.Context) {
+	name := c.Param("name")
+	c.JSON(http.StatusOK, gin.H{"message": "Hello, " + name + "!"})
+}
+
 func main() {
-	// Create a Gin router
 	router := gin.Default()
 
-	// Define a route handler
-	router.GET("/", func(c *gin.Context) {
-		c.JSON(http.StatusOK, gin.H{"message": "Hello, You Created a Web App!"})
-	})
+	// Define a route handler for the root path
+	router.GET("/", helloCall)
 
-	// Run the server on port 8080
+	// Define a route handler for "/about"
+	router.GET("/about", aboutCall)
+
+	// Define a route handler for "/contact"
+	router.GET("/contact", contactCall)
+
+	// Add this route handler
+	// with param
+	router.GET("/user/:name", getUser)
+
 	router.Run(":3000")
 }
